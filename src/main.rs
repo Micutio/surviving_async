b766989a1bbdfa1bc2173b65113b1159c44d39f4
michael wagner <michael.wagner@tum-create.edu.sg>
2020-08-28 00:21:25 +0800

    added: first async iteration

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,70 @@
 use argh::FromArgs;
+use async_std::{fs::File, io::ReadExt};
+
 use color_eyre::eyre;
+use futures::io::AsyncRead;
 use sha3::Digest;
-use std::{fs::File, io::Read, path::PathBuf};
+use std::io;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::path::{Path, PathBuf};
 
 /// Prints the SHA-256 hash of a file.
 #[derive(FromArgs)]
 struct Args {
     /// the file whose contents to hash and print
     #[argh(positional)]
-    file: PathBuf,
+    files: Vec<PathBuf>,
 }
 
-fn main() -> Result<(), eyre::Error> {
+struct TracingReader<R> where R: AsyncRead {
+    inner: R,
+}
+
+impl<R> AsyncRead for TracingReader<R> where R:AsyncRead {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<io::Result<usize>> {
+        todo!();
+    }
+}
+
+#[async_std::main]
+async fn main() -> Result<(), eyre::Error> {
     color_eyre::install().unwrap();
     let args: Args = argh::from_env();
 
-    let mut file = File::open(&args.file)?;
+    let mut handles = Vec::new();
+
+    for file in &args.files {
+        let file = file.clone();
+        let handle = async_std::task::spawn(async move {
+            let res = hash_file(&file).await;
+            if let Err(e) = res {
+                println!("WHile hashing {}: {}", file.display(), e);
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.await;
+    }
+
+    Ok(())
+}
+
+async fn hash_file(path: &Path) -> Result<(), eyre::Error> {
+    let mut file = File::open(path).await?;
     let mut hasher = sha3::Sha3_256::new();
 
-    let mut buf = vec![0u8; 256*1024];
+    let mut buf = vec![0u8; 256 * 1024];
     loop {
-        let n = file.read(&mut buf[..])?;
+        // check out the current thread id
+        println!("{} => {:?}", path.display(), std::thread::current().id());
+        let n = file.read(&mut buf[..]).await?;
         match n {
             0 => break,
             n => hasher.update(&buf[..n]),
@@ -28,7 +72,7 @@ fn main() -> Result<(), eyre::Error> {
     }
 
     let hash = hasher.finalize();
-    print!("{} ", args.file.display());
+    print!("{} ", path.display());
     for x in hash {
         print!("{:02x}", x);
     }
@@ -36,3 +80,4 @@ fn main() -> Result<(), eyre::Error> {
 
     Ok(())
 }
+
